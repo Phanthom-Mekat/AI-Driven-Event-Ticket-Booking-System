e6a2a0a21e9f5106031d9c7253aef2a7d4ce3f14
Phanthom-Mekat <jahidulhossainmekat@gmail.com>
2025-03-26 21:56:16 +0600

    basic credential setup done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Sora } from "next/font/google";
 import "@/app/globals.css";
-
+import {SessionProvider} from "next-auth/react";
 const sora = Sora({
   subsets: ["latin"],
   variable: "--font-sora",
@@ -17,10 +17,12 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
+    <SessionProvider>
     <html lang="en">
       <body className={`${sora.variable} antialiased bg-gray-50 text-gray-900`}>
         {children}
       </body>
     </html>
+    </SessionProvider>
   );
 }
