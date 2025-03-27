b14376be3e3d2db394d7f6496b8f979a5442ce3b
Phanthom-Mekat <jahidulhossainmekat@gmail.com>
2025-03-28 03:36:46 +0600

    google ,github auth with database

---
+++ src/auth.ts
@@ -0,0 +1,15 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import NextAuth from "next-auth";
+import { Adapter } from "next-auth/adapters";
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+import prisma from "./lib/prisma";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma) as Adapter,
+
+  providers: [
+    Google,
+    GitHub,
+  ],
+});
