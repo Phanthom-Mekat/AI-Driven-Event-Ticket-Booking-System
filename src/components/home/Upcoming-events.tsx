1800b5dadf8e69a6e2446f31c560a95a83158a6a
Sanjida <sanjida.ice.778@gmail.com>
2025-03-29 15:44:18 +0600

    image uploaded locally in upcoming events section

--- src/components/home/Upcoming-events.tsx
+++ src/components/home/Upcoming-events.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { Card, Carousel } from "../apple-cards-carousel";
 
+
 export function UpcomingEvents() {
   const cards = data.map((card, index) => (
     <Card key={card.src} card={card} index={index} />
@@ -35,7 +36,7 @@ const data = [
     location: "Miami Beach, Florida",
     description:
       "A weekend of electrifying performances by top rock bands and artists on the sandy shores of Miami Beach.",
-    src: "https://i.imgur.com/Qofj3lc.jpeg",
+    src: "/upcoming-events-img/image2.jpg",
     ticketsSold: 12000,
     slug: "rock-on-the-beach",
   },
@@ -47,7 +48,7 @@ const data = [
     location: "Paris, France",
     description:
       "Explore groundbreaking works from emerging artists at the Modern Art Expo in the heart of Paris.",
-    src: "https://i.imgur.com/7uD9AQL.jpeg",
+    src: "/upcoming-events-img/image1.jpg",
     ticketsSold: 3200,
     slug: "modern-art-expo",
   },
@@ -58,7 +59,7 @@ const data = [
     location: "San Francisco, California",
     description:
       "Learn the latest coding skills and tech trends at this intensive bootcamp for aspiring developers and engineers.",
-    src: "https://i.imgur.com/8FQMcGK.png",
+    src: "/upcoming-events-img/image3.png",
     ticketsSold: 500,
     slug: "tech-coding-bootcamp",
   },
@@ -69,7 +70,7 @@ const data = [
     location: "London, UK",
     description:
       "Catch the best tennis players in action at the prestigious Grand Slam Tennis Tournament in London.",
-    src: "https://i.imgur.com/dRZ64br.png",
+    src: "/upcoming-events-img/image7.png",
     ticketsSold: 7000,
     slug: "grand-slam-tennis",
   },
@@ -80,7 +81,7 @@ const data = [
     location: "New York, USA",
     description:
       "An exclusive event for business leaders and entrepreneurs to expand their networks and share insights.",
-    src: "https://i.imgur.com/Jh2sB3q.jpeg",
+    src: "/upcoming-events-img/image4.jpg",
     ticketsSold: 1500,
     slug: "business-networking-gala",
   },
@@ -91,7 +92,7 @@ const data = [
     location: "Berlin, Germany",
     description:
       "Celebrate global diversity through music, food, and art at the Global Cultural Festival in Berlin.",
-    src: "https://i.imgur.com/7paRx5T.png",
+    src: "/upcoming-events-img/image6.png",
     ticketsSold: 10000,
     slug: "global-cultural-fest",
   },
@@ -102,7 +103,7 @@ const data = [
     location: "Los Angeles, California",
     description:
       "Enjoy a weekend of your favorite movies, from classic hits to the latest blockbusters, with friends and family.",
-    src: "https://images.unsplash.com/photo-1602081957921-9137a5d6eaee?q=80&w=2793&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    src: "/upcoming-events-img/image8.png",
     ticketsSold: 2000,
     slug: "movie-marathon-weekend",
   },
