14b41855846497e15d052f90e2595c823983a4cf
aliakborswe <aliakborswe@gmail.com>
2025-03-29 15:29:01 +0600

    add: user management page done

--- src/app/(dashboard)/dashboard/(adminDashboard)/user-management/page.tsx
+++ src/app/(dashboard)/dashboard/(adminDashboard)/user-management/page.tsx
@@ -1,11 +1,319 @@
-import React from 'react';
+"use client";
+import Spinner from "@/components/common/Spinner";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Input } from "@/components/ui/input";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  SortingState,
+  useReactTable,
+  VisibilityState,
+} from "@tanstack/react-table";
+import { ChevronDown, Trash2 } from "lucide-react";
+import Image from "next/image";
+import React, { useState } from "react";
+import { toast } from "react-toastify";
+import Swal from "sweetalert2";
+
+type User = {
+  _id: string;
+  name: string;
+  email: string;
+  image: string;
+};
+
+const fackUsers: User[] = [
+  {
+    _id: "1",
+    name: "Alice Johnson",
+    email: "alice@example.com",
+    image: "/image/img1.jpg",
+  },
+  {
+    _id: "2",
+    name: "Bob Smith",
+    email: "bob@example.com",
+    image: "/image/img1.jpg",
+  },
+  {
+    _id: "3",
+    name: "Charlie Brown",
+    email: "charlie@example.com",
+    image: "/image/img1.jpg",
+  },
+  {
+    _id: "4",
+    name: "David Wilson",
+    email: "david@example.com",
+    image: "/image/img1.jpg",
+  },
+  {
+    _id: "5",
+    name: "Emma White",
+    email: "emma@example.com",
+    image: "/image/img1.jpg",
+  },
+];
 
 const UserManagementPage = () => {
+  const [users, setUsers] = useState<User[]>(fackUsers);
+  const [loading] = useState(false);
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
+  const [rowSelection, setRowSelection] = useState({});
+
+  const columns: ColumnDef<User>[] = [
+    {
+      id: "select",
+      header: ({ table }) => (
+        <Checkbox
+          checked={
+            table.getIsAllPageRowsSelected() ||
+            (table.getIsSomePageRowsSelected() && "indeterminate")
+          }
+          onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+          aria-label='Select all'
+        />
+      ),
+      cell: ({ row }) => (
+        <Checkbox
+          checked={row.getIsSelected()}
+          onCheckedChange={(value) => row.toggleSelected(!!value)}
+          aria-label='Select row'
+        />
+      ),
+      enableSorting: false,
+      enableHiding: false,
+    },
+    {
+      accessorKey: "image",
+      header: "User Image",
+      cell: ({ row }) => (
+        <Image
+          src={row.getValue("image")}
+          alt={row.getValue("name")}
+          width={32}
+          height={32}
+          className='w-8 h-8 rounded-full'
+        />
+      ),
+    },
+    {
+      accessorKey: "name",
+      header: "User Name",
+      cell: ({ row }) => <div>{row.getValue("name")}</div>,
+    },
+    {
+      accessorKey: "email",
+      header: "User Email",
+      cell: ({ row }) => <div>{row.getValue("email")}</div>,
+    },
+    {
+      accessorKey: "action",
+      header: "Action",
+      cell: ({ row }) => {
+        const id = row.original._id; // Get the id of the current row
+        return (
+          <span
+            onClick={() => handleDelete(id)}
+            className='cursor-pointer text-red-500 pl-2'
+          >
+            <Trash2 />
+          </span>
+        );
+      },
+    },
+  ];
+
+  const table = useReactTable<User>({
+    data: users,
+    columns,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      rowSelection,
+    },
+  });
+
+  // handle Delete button
+  const handleDelete = async (id: string) => {
+    try {
+      Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!",
+      }).then((result: { isConfirmed: boolean }) => {
+        if (result.isConfirmed) {
+          setUsers((prevData) => prevData.filter((event) => event._id !== id));
+          alert(`Delete ${id}`);
+        }
+      });
+    } catch (err: unknown) {
+      toast.error(
+        err instanceof Error ? err.message : "An unknown error occurred"
+      );
+    }
+  };
+
+  if (loading) {
+    return <Spinner />;
+  }
+  if ((users?.length ?? 0) === 0) {
     return (
-        <div>
-            User Management page
-        </div>
+      <div className='flex justify-center items-center'>
+        <h1 className='text-3xl font-bold text-red-500'>No data found</h1>
+      </div>
     );
+  }
+
+  return (
+    <div className='w-full p-6'>
+      <h1 className='text-2xl font-bold'>User Management</h1>
+      <div className='flex gap-4 items-center py-4'>
+        <Input
+          placeholder='Search by User Name...'
+          value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
+          onChange={(event) =>
+            table.getColumn("name")?.setFilterValue(event.target.value)
+          }
+          className='max-w-xs'
+        />
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant='outline' className='ml-auto'>
+              Columns <ChevronDown />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align='end'>
+            {table
+              .getAllColumns()
+              .filter((column) => column.getCanHide())
+              .map((column) => {
+                return (
+                  <DropdownMenuCheckboxItem
+                    key={column.id}
+                    checked={column.getIsVisible()}
+                    onCheckedChange={(value) =>
+                      column.toggleVisibility(!!value)
+                    }
+                  >
+                    {column.id}
+                  </DropdownMenuCheckboxItem>
+                );
+              })}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+      <div className='rounded-md border'>
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className='h-24 text-center'
+                >
+                  No results.
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+      <div className='flex items-center justify-end space-x-2 py-4'>
+        <div className='flex-1 text-sm text-muted-foreground'>
+          {table.getFilteredSelectedRowModel().rows.length} of{" "}
+          {table.getFilteredRowModel().rows.length} row(s) selected.
+        </div>
+        <div className='space-x-2'>
+          <Button
+            variant='outline'
+            size='sm'
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
+            Previous
+          </Button>
+          <Button
+            variant='outline'
+            size='sm'
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
+            Next
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default UserManagementPage;
