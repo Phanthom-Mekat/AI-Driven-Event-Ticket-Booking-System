ce3d9d0720e220ef5416a41d8e0c144135c88b08
Shorno <fb.shorno@gmail.com>
2025-03-29 15:56:19 +0600

    fix minor issues

--- src/auth.ts
+++ src/auth.ts
@@ -1,15 +1,22 @@
-import { PrismaAdapter } from "@auth/prisma-adapter";
+import {PrismaAdapter} from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
-import { Adapter } from "next-auth/adapters";
+import {Adapter} from "next-auth/adapters";
 import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 import prisma from "./lib/prisma";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  adapter: PrismaAdapter(prisma) as Adapter,
+export const {handlers, signIn, signOut, auth} = NextAuth({
+    adapter: PrismaAdapter(prisma) as Adapter,
 
-  providers: [
-    Google,
-    GitHub,
-  ],
+    providers: [
+        Google,
+        GitHub,
+    ],
+    callbacks: {
+        session({session, user}) {
+            session.user.id = user.id
+            return session
+        },
+    }
+
 });
